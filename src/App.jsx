46807d2f1e7ef9029e13d9b9c4ec73b01cbefe8b
Tanam179 <vonam235689741.work@gmail.com>
2024-06-30 01:19:23 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+    font-size: 30px;
+    font-weight: 600;
+    text-transform: uppercase;
+`
+
+const App = () => {
+    return (
+        <H1>
+            The Wild Oasis
+        </H1>
+    );
+};
+
+export default App;
